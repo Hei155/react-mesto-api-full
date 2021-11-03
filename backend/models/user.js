30001794117536a537ad78bc86ba48050e1ec4e7
Igor Romanov <Hei155@yandex.ru>
2021-11-04 00:50:41 +0300

    Second

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const { isEmail, isURL } = require('validator/lib/isEmail');
+const isEmail = require('validator/lib/isEmail');
+const isURL = require('validator/lib/isURL');
 
 const userSchema = new mongoose.Schema({
   name: {
